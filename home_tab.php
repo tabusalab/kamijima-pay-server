ca3a2ee11cc8824531419f5bca5d58bf8f26c6b7
oka0914 <minami.oka914@gmail.com>
2020-02-19 14:01:53 +0900

    first commit

---
+++ home_tab.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang = "ja">
+
+
+
+
+
+<head>
+	<title>上島Pay</title>
+</head>
+
+
+
+<body>
+	<center>
+
+	<font size="5">
+		<h1>上島Pay</h1>
+	</font>
+
+<form action="seigyo.php" method="post">
+	<p>
+		<br>
+		<br>
+		<button type="submit" name="list" class="simple_square_btn7"　>チャージ</button>
+		<br>
+
+		<button type="submit" name="home" class="simple_square_btn7"　>履歴</button>
+	</p>
+</form>
+
+</center>
+</body>
+</div>
+</html>
+
