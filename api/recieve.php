96c76796a4ce9563b0d904001b250b04c1de032a
miyako623 <miyako.385623@gmail.com>
2020-02-20 14:21:01 +0900

    履歴テーブルの残高を更新する仕様に変更

--- api/recieve.php
+++ api/recieve.php
@@ -48,8 +48,36 @@ if($data === null){
         $productID = $data["productID"];
         $date = date('Y-m-d H:i:s');
 
-        $query = "INSERT INTO history_tab(userid,shopid,productid,datetime) VALUES('$userID','$shopID','$productID','$date')";
+        if($data["price"]==null){
+
+            
+            $queryprice="SELECT * FROM product_tab WHERE productid=$productID";
+            $stmt2 = $dbh->query($queryprice);
+            $rec = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+            foreach($rec as $row){
+                $price=$row['product_price'];
+            } 
+        }else{
+
+            $price = $data["price"];
+        }
+        
+        $querybalance = "UPDATE money_tab SET balance=balance-$price WHERE userid=$userID";
+        $stmt1 = $dbh->query($querybalance);
+        $queryreadbal = "SELECT * FROM money_tab WHERE userid=$userID";
+        $stmtb = $dbh->query($queryreadbal);
+        $recbal = $stmtb->fetchAll(PDO::FETCH_ASSOC);
+        foreach($recbal as $rowbal){
+            $balance=$rowbal["balance"];
+        }
+        if($data["price"]==null){
+            $query = "INSERT INTO history_tab(userid,shopid,productid,balance,datetime) VALUES('$userID','$shopID','$productID',$balance,'$date')";
+        }else{
+            $query = "INSERT INTO history_tab(userid,shopid,productid,price,balance,datetime) VALUES('$userID','$shopID','$productID','$price',$balance,'$date')";
+
+        }
         $stmt = $dbh->query($query);
+        echo json_encode("success");
         
     }catch(PDOException $e){
         echo $e->getMessage();
